8e0a8fa843eab12f561a76821875af80c8c33702
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2024-09-17 23:18:40 -0600

    Update OperacionesAritmeticas.c

--- OperacionesAritmeticas05.c
+++ OperacionesAritmeticas05.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main(){
-	print
-	f("La suma de %d + %d es = %d \n",5 ,8, 5+8);
-	printf("La suma de %d + %d es = %d \n", 78787, 3259, 78787+3259);
-	system("Pause");
-	return 0;
+    int suma = 5+2; // =7
+    printf("%d\n",suma);
+    int resta = suma - 3; // =4
+    printf("%d\n",resta);
+    int mult = suma * resta; // = 28
+    printf("%d\n",mult);
+    int iDiv = 20/8; // = 2
+    printf("%d\n",iDiv);
+    int mod = 20 % 8; // = 4 
+    printf("%d\n",mod);
+    int a = 2;
+    ++a; //a = 3
+    printf("a preIncremento: %d\n",a);
+    a++; //a=4
+    printf("a postIncremento: %d\n",a);
+    int b = a++; //b=4, a=5
+    printf("b: %d , a: %d\n",b,a);
+    int c = ++b; //c =5, b = 5
+    printf("c: %d , b: %d\n",c,b);
+    return 0;
 }
