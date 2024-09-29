ad765eedcc5b7271c08d05f7282f30016514a348
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2024-09-28 20:26:06 -0600

    Saber si un numero es mayor

---
+++ NumeroMayor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int numero = 98;
+    if(numero<50){
+        printf("El numero es menor que 50\n");
+    }else{
+        printf("El numero es mayor que 50\n");
+    }
+    printf("Fin del programa \n");
+    return 0;
+}
+
