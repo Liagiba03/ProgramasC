bf5c2134faa3227504fdf7faf805d4ac546aded5
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2024-09-28 20:24:32 -0600

    Ejercicio if simple

---
+++ E01_IfSimple.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x=1;
+    if(x==1){
+        printf("Todo este\n");
+        printf("código, se imprime dentro\n");
+        printf("del bloque if\n");
+    }
+    return 0;
+}
+
