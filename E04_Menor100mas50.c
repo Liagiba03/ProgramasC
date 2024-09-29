06450c593aa9d065e4da31da26dbb0529e5d9d9f
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2024-09-28 20:25:44 -0600

    Caso if simple

---
+++ E04_Menor100mas50.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int numero = 55;
+    if(numero<100){
+        printf("El numero es menor que 100\n");
+        if(numero>50)
+        printf(" y mayor que 50\n");
+    }
+    printf("Fin del programa \n");
+    return 0;
+}
+
