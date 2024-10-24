89d24f7a6ac7bb45154688a77363dc4ff81c5f2a
Suseth Abigail Sandoval Damian <143125742+Liagiba03@users.noreply.github.com>
2024-10-23 23:24:43 -0600

    Imprimir de 1 a 500 cuadrados y cubos de números

---
+++ ImprimirCuadrados.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int i =1;
+    while(i<=500){
+        printf("%d, %d, %d\n",i,(i*i),(i*i*i));
+        i++;
+    }
+    printf("Fin del programa");
+    return 0;
+}
+
+
