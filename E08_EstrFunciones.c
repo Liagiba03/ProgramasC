3847f594dfb5dfa6df9395e2a7553fc6eef7776b
Suseth Abigail Sandoval Damian <143125742+Liagiba03@users.noreply.github.com>
2024-12-25 23:48:52 -0600

    Struct con funciones

---
+++ E08_EstrFunciones.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct fraccion{
+    int den;
+    int num;
+};
+int multipli(struct fraccion mul1, struct fraccion mul2);
+int main(){
+    struct fraccion frac1;
+    struct fraccion frac2;
+    frac1.den =3;
+    frac1.num= 4;
+    frac2.den = 6;
+    frac2.num = 2;
+    multipli(frac1,frac2);
+    return 0;
+}
+int multipli(struct fraccion mul1, struct fraccion mul2){
+    int den = mul1.den * mul2.den;
+    int nom = mul1.num * mul2.num;
+    printf("%d/%d",den,nom);
+    return 0;
+}
