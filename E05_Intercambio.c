045b01812be11725100874b9240ea4c5a69e6cc8
Suseth Abigail Sandoval Damian <143125742+Liagiba03@users.noreply.github.com>
2025-01-01 23:38:03 -0600

    Ejemplo intercambio de memoria-Apuntadores

---
+++ E05_Intercambio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void intercambio(int *a, int *b);
+int main(){
+    int x=2;
+    int y= 5;
+    printf("Antes x = %d, y= %d \n",x,y);
+    intercambio(&x,&y);
+    printf("Despues x = %d, y = %d \n",x,y);
+    return 0;
+}
+
+void intercambio(int *a, int *b){
+    int temp;
+    temp = *b;
+    *b= *a;
+    *a = temp;
+}
